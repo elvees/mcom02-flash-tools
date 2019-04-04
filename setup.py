7e953d79a43a929b736cb2627fbfb3e22604194e
Olzhas Ivanov <oivanov@elvees.com>
2019-04-04 15:25:32 +0300

    Remove mcom02_flash_mmc.py
    The utility is useless and can be replaced with 'dd .. | ssh ..' command.
    
    Issue: #MCOM02SW-1283
    Change-Id: I25fce32babb36c532df1d25c0fb00ed9662ef2f6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 #
-# Copyright 2017-2018 RnD Center "ELVEES", JSC
+# Copyright 2017-2019 RnD Center "ELVEES", JSC
 #
 # SPDX-License-Identifier: MIT
 #
@@ -12,10 +12,8 @@ setup(
     version='2.2.0',
     description='MCom-02 based PCB flashing tools',
     python_requires='~=2.7',
-    scripts=['mcom02_flash_mmc.py',
-             'mcom02_flash_spi.py',
+    scripts=['mcom02_flash_spi.py',
              'mcom02_flash_ums_mmc.py'],
     install_requires=['intelhex>=2.1,<3.0',
-                      'paramiko==2.2.1',
                       'pyserial>=3.0,<4.0'],
 )
