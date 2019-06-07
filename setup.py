elvees/mcom02-flash-tools

commit 078d46907983a32c4b96cc4b3cc3c3c061d25721

Move common UART functions to package
Both scripts use UART for run commands. This functionality is
unified and moved to separate package.

Change-Id: I25c9249eae2a3e9185d6152a330cbac166990b63

setup.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: MIT
 #
 
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
@@ -12,8 +13,10 @@ setup(
     version='2.2.0',
     description='MCom-02 based PCB flashing tools',
     python_requires='~=2.7',
+    packages=find_packages(),
     scripts=['mcom02_flash_spi.py',
              'mcom02_flash_ums_mmc.py'],
     install_requires=['intelhex>=2.1,<3.0',
+                      'monotonic>=1.0,<2.0',
                       'pyserial>=3.0,<4.0'],
 )

mcom02_flash_spi.py
@@ -11,29 +11,16 @@ import platform
 from StringIO import StringIO
 import struct
 import sys
-import time
 
 from intelhex import IntelHex
-import serial
+from serial import SerialException
+
+from mcom02_flash_tools import UART
 
 __version__ = '2.2.0'
 
-def wait_new_command_line(tty, timeout=10):
-    s = ""
-    time_stop = time.time() + timeout
-    while not s.endswith("\r#"):
-        char = tty.read()
-        if time.time() > time_stop:
-            return None
-        if char:
-            s += char
-
-    return s
-
-
 def send_cmd(tty, cmd):
-    tty.write(cmd + "\n")
-    res = wait_new_command_line(tty)
+    res = tty.run(cmd, timeout=10, strip_echo=False)
     if res is None:
         print "Error: the device does not respond on {}".format(cmd)
         sys.exit(1)
@@ -46,7 +33,6 @@ def send_cmd(tty, cmd):
     if cmd not in res:
         print "Error: BootROM received incorrect command arguments:\n{}".format(res)
         sys.exit(1)
-    tty.flush()
     return res
 
 
@@ -54,8 +40,7 @@ def send_ihex(tty, ihex):
     sio = StringIO()
     ihex.write_hex_file(sio)
     sio.seek(0)
-    tty.write(sio.read())
-    res = wait_new_command_line(tty)
+    res = tty.run(sio.read().strip())
     if res is None:
         print "Error: the device does not respond on writing a file."
         sys.exit(1)
@@ -110,7 +95,7 @@ def dump2bytes(list_string):
 def check_block(tty, data, offset, size):
     dump_count = int((size + 3) / 4)
     dump = send_cmd(tty, "dumpspiflash {:x} {:x}".format(offset, dump_count))
-    received = dump2bytes(dump.split("\n\r")[2:][:-1])
+    received = dump2bytes(dump.split("\n")[2:][:-1])
     return received[:size] == data[offset:][:size]
 
 
@@ -158,13 +143,12 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        tty = serial.Serial(port=args.port, baudrate=115200, timeout=0.5)
-    except serial.SerialException:
+        tty = UART(prompt='\r#', port=args.port)
+    except SerialException:
         print "Error: cannot open the device '%s'." % args.port
         sys.exit(1)
 
-    tty.write("\n")
-    if wait_new_command_line(tty) is None:
+    if tty.run("") is None:
         print "Error: terminal does not respond. Set the boot mode to UART " \
               "and reset the board power (do not use warm reset)."
         sys.exit(1)

mcom02_flash_ums_mmc.py
@@ -12,7 +12,7 @@ import subprocess
 import sys
 import time
 
-import serial
+from mcom02_flash_tools import UART
 
 __version__ = '2.2.0'
 
@@ -24,55 +24,8 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def wait_for_string(tty, expected, timeout=10):
-    time_end = time.time() + timeout
-
-    def wait():
-        if not timeout:
-            return True
-        return time.time() <= time_end
-
-    resp = ''
-    while wait():
-        ch = tty.read(1)
-        if (ch is None) or (ch == ''):
-            continue
-        if resp.endswith(expected):
-            return True, resp
-        resp += ch
-
-    return False, resp
-
-
-def get_uboot_board_model(tty, prompt):
-    run_command(tty, "fdt addr $fdtcontroladdr", prompt)
-    model = run_command(tty, "fdt list / model", prompt)
-    try:
-        return model.split('=')[1].strip()
-    except Exception:
-        return None
-
-
-def uboot_break(tty, prompt):
-    run_command(tty, '\x03', prompt)  # send Ctrl-C to stop probably running process
-
-
-def get_uboot_version(tty, prompt):
-    version = run_command(tty, 'version', prompt)
-    try:
-        return [x.strip() for x in version.split('\n') if x.startswith('U-Boot')][0]
-    except Exception:
-        return None
-
-
-def run_command(tty, cmd, prompt, timeout=exp_str_timeout):
-    tty.reset_input_buffer()
-    tty.write('{}\n'.format(cmd))
-    success, resp = wait_for_string(tty, prompt, timeout)
-    if not success:
-        raise IOError('UART timeout')
-    # Return only output of command (without cmd + '\r\n' and command prompt)
-    return resp[len(cmd) + 2: -len(prompt)]
+def uboot_break(tty):
+    tty.run('\x03', timeout=exp_str_timeout)  # send Ctrl-C to stop probably running process
 
 
 def get_block_devices():
@@ -106,28 +59,29 @@ if __name__ == '__main__':
     parser.add_argument('--version', action='version', version=__version__)
     args = parser.parse_args()
 
-    tty = serial.Serial(port=args.port, baudrate=115200, timeout=0.5)
-    ok, _ = wait_for_string(tty, 'Hit any key to stop autoboot', timeout=args.wait_uboot)
+    tty = UART(prompt=args.prompt, port=args.port)
+    wait_uboot = None if not args.wait_uboot else args.wait_uboot
+    ok, _ = tty.wait_for_string('Hit any key to stop autoboot', timeout=wait_uboot)
     if not ok:
         eprint('Error: U-Boot terminal does not respond. Set the boot mode to SPI '
                'and reset the board power (do not use warm reset).')
         sys.exit(1)
-    tty.write('a')  # hitting key to stop autoboot
-    wait_for_string(tty, args.prompt)
+    tty.run('')  # hitting key to stop autoboot
 
-    uboot_version = get_uboot_version(tty, args.prompt)
+    uboot_version = tty.get_uboot_version()
     if uboot_version is not None:
         print('Found U-Boot: {}'.format(uboot_version))
     else:
         eprint('Error: no U-Boot terminal found.')
         sys.exit(1)
 
-    print('Board model: ', get_uboot_board_model(tty, args.prompt))
+    print('Board model: ', tty.get_uboot_board_model(timeout=exp_str_timeout))
     print('Enabling USB Mass storage on target...')
     block_device_list_before_init = get_block_devices()
-    tty.write('ums 0 mmc {}\n'.format(args.mmcdev))
+    tty.tty.write('ums 0 mmc {}\n'.format(args.mmcdev))
     time.sleep(usb_device_init_delay)
-    ok, resp = wait_for_string(tty, 'UMS: LUN 0, dev {}'.format(args.mmcdev))
+    ok, resp = tty.wait_for_string('UMS: LUN 0, dev {}'.format(args.mmcdev),
+                                   timeout=exp_str_timeout)
     if not ok:
         eprint('Error: failed to enable UMS for MMC {}. U-Boot response {}.'
                .format(args.mmcdev, resp))
@@ -161,5 +115,5 @@ if __name__ == '__main__':
         eprint('Error: error while writing image to USB device')
         sys.exit(1)
 
-    uboot_break(tty, args.prompt)
+    uboot_break(tty)
     print('Done')

mcom02_flash_tools/__init__.py
@@ -1,0 +1,157 @@
+# Copyright 2019 RnD Center "ELVEES", JSC
+
+from __future__ import print_function
+import sys
+
+import monotonic
+import serial
+
+
+class CommandError(Exception):
+    pass
+
+
+class UART(object):
+    """Class for work with UART console.
+    """
+
+    def __init__(self, prompt, port, newline='\n', verbose=False, baudrate=115200, timeout=0.5):
+        """Parameters
+        ----------
+        prompt : str
+            expected command line prompt
+        port : str
+            serial port for use (example: /dev/ttyUSB0)
+        newline : str
+            new line delimeter
+        verbose : bool
+            if True then will show UART transactions
+        baudrate : int
+            UART speed in bit/sec
+        timeout : float
+            timeout for read() operations and affects the accuracy of the command execution time
+        """
+        self.prompt = prompt
+        self.newline = newline
+        self.verbose = verbose
+        self.tty = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+
+    def wait_for_string(self, expected, timeout=1):
+        """Method to wait for pattern `expected` to be received from UART.
+
+        Parameters
+        ----------
+        expected : list or str
+            string or list of strings for wait.
+        timeout : float
+            time in seconds. If function does not receive string `expected` in this time then will
+            return with False. If None then will wait infinite for expected string.
+
+        Returns
+        -------
+        bool
+            True if pattern `expected` was received
+        str
+            received data
+        """
+
+        def endswith(resp, expected):
+            if not isinstance(expected, (list, tuple)):
+                expected = (expected, )
+
+            return any(map(resp.endswith, expected))
+
+        if timeout is not None:
+            time_end = monotonic.monotonic() + timeout
+        else:
+            time_end = sys.float_info.max
+        resp = ''
+        while (monotonic.monotonic() <= time_end) and not endswith(resp, expected):
+            ch = self.tty.read(1)
+            if not ch:
+                continue
+            resp += ch
+
+        result = resp.replace('\r', '')
+        if self.verbose:
+            print(result, end='')
+        if not endswith(resp, expected):
+            return False, result
+
+        return True, result
+
+    def run(self, cmd, timeout=5, strip_echo=True):
+        """Run command and wait for prompt.
+
+        Parameters
+        ----------
+        cmd : str
+            command
+        timeout : float
+            argument for wait_for_string()
+        strip_echo : bool
+            if true then will remove echo from response string
+
+        Returns
+        -------
+        str
+            response string
+        """
+        self.tty.write('{}{}'.format(cmd, self.newline))
+        success, resp = self.wait_for_string(self.prompt, timeout)
+        if not success:
+            return None
+
+        # Return only output of command (without cmd + "\n" and command prompt)
+        return resp[len(cmd) + 1: -len(self.prompt)] if strip_echo else resp
+
+    def run_with_retcode(self, cmd, timeout=5, strip_echo=True, check=True, errmsg=None):
+        """Run command and wait for prompt. Return retcode and result.
+
+        Parameters
+        ----------
+        cmd : str
+            command
+        timeout : float
+            argument for wait_for_string()
+        strip_echo : bool
+            if true then will remove echo from response string
+        check : bool
+            if True then will raise exception if command return non-zero retcode
+        errmsg : string
+            message for output when received non-zero retcode (used only with check=True)
+
+        Returns
+        -------
+        int
+            return code of command
+        str
+            response string
+        """
+        result = self.run(cmd, timeout, strip_echo)
+        resp = self.run('echo $?')
+        if resp is None:
+            raise CommandError('UART timeout at command "echo $?" after "{}"'.format(cmd))
+
+        retcode = int(resp)
+        if check and retcode:
+            if errmsg is None:
+                errmsg = 'Command "{}" failed'.format(cmd)
+            raise CommandError('{}\nTarget answer:\n{}'.format(errmsg, result))
+
+        return retcode, result
+
+    def get_uboot_board_model(self, timeout=5):
+        self.tty.run("fdt addr $fdtcontroladdr", timeout=timeout)
+        model = self.tty.run("fdt list / model", timeout=timeout)
+        try:
+            return model.split('=')[1].strip()
+        except Exception:
+            return None
+
+    def get_uboot_version(self):
+        version = self.tty.run('version')
+        try:
+            return [x.strip() for x in version.split('\n') if x.startswith('U-Boot')][0]
+        except Exception:
+            return None

commit ea021f73fb6ee645a685830f018365d7a2ea7f6d

Move all scripts to package directory
Commit 078d46907 introduced two errors:

* created __init__.py file contained an error which was fixed
  in follow-up commit
* __init__.py was not added to tox.ini
  hence first error was nor detected by Pylint

This commit fixes second error: we move all scripts to
mcom02_flash_tools directory and add that directory to pylint.
Once new files are added to that directory no modifications to
tox.ini are required.

Change-Id: I1bf64d6e31481c054476d32fdae429fd31270e62

setup.py
@@ -14,8 +14,8 @@ setup(
     description='MCom-02 based PCB flashing tools',
     python_requires='~=2.7',
     packages=find_packages(),
-    scripts=['mcom02_flash_spi.py',
-             'mcom02_flash_ums_mmc.py'],
+    scripts=['mcom02_flash_tools/mcom02_flash_spi.py',
+             'mcom02_flash_tools/mcom02_flash_ums_mmc.py'],
     install_requires=['intelhex>=2.1,<3.0',
                       'monotonic>=1.0,<2.0',
                       'pyserial>=3.0,<4.0'],

tox.ini
@@ -12,7 +12,7 @@ deps = pylint>=1.6
 commands = flake8 .
 
 [testenv:pylint]
-commands = bash -c 'pylint -E {toxinidir}/*.py'
+commands = bash -c 'pylint -E {toxinidir}/*.py {toxinidir}/mcom02_flash_tools'
 whitelist_externals = bash
 
 [flake8]